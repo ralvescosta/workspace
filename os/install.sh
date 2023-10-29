a5719529d6ce1b5aa6703ca5b1c8f132838c6403
ralvescosta <rafael.rac.mg@gmail.com>
2023-10-29 15:30:45 -0300

    feat: update nerd font

--- os/install.sh
+++ os/install.sh
@@ -212,7 +212,7 @@ echo '===============================\n'
 echo 'Installing Nerd Font...\n'
 echo '===============================\n'
 mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 fc-cache -fv
 clear
 
